25064b5b0ba3e922eaa8dd5eff60a36b2f4368b7
SP-MMIBdx <stephen.pedder@mmibordeaux.com>
2025-09-25 11:36:07 +0200

    Add edit fuctionality .blade

---
+++ oceanside/resources/views/Rentals/edit.blade.php
@@ -0,0 +1,29 @@
+<h1>Edit a Rental</h1>
+
+<form action="{{ route ('rentals.update', $rentals->id) }}" method="POST">
+    @csrf
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" value="{{ $rentals->name }}"><br><br>
+
+    <label for="location">Location:</label>
+    <input type="text" id="type" name="type" value="{{ $rentals->location }}"><br><br>
+
+    <label for="price_per_night">Price Per Night:</label>
+    <input type="number" id="price_per_night" name="price_per_night" value="{{ $price_per_night->price_per_night }}"><br><br>
+
+    <label for="nomber_of_rooms">Number of Rooms:</label>
+    <input type="number" id="nomber_of_rooms" name="nomber_of_rooms" value="{{ $nomber_of_rooms->nomber_of_rooms }}"><br><br>
+
+    <label for="equipment">Equipment:</label>
+    <input type="text" id="equipment" name="equipment" value="{{ $equipment->equipment }}"><br><br>
+
+    <label for="description">Description:</label>
+    <input type="text" id="description" name="description" value="{{ $description->description }}"><br><br>
+
+
+    <label for="image_url">Image URL:</label>
+    <input type="text" id="image_url" name="image_url"><br><br>
+
+    <input type="submit" value="Edit Rental">
+    
+</form>
