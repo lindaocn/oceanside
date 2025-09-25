20bce76b4227945d002374be1a37bbee8bf09e7b
lindaocn <lilienhelpss@web.de>
2025-09-25 17:10:12 +0200

    bootstrap link in app.blade

--- oceanside/resources/views/Rentals/all.blade.php
+++ oceanside/resources/views/Rentals/all.blade.php
@@ -1,15 +1,16 @@
+
 @extends('app')
 @section('content')
-
-<h1>Current Rentals</h1>
-<u>
-    @foreach ($rentals as $rental)
-        <li>
-            <a href="{{ route('rentals.show', $rental->id) }}">
-                {{ $rental->name }} - {{ $rental->location }} - € {{ $rental->price_per_night }} per night - {{ $rental->nomber_of_rooms }} rooms - {{ $rental->description }} - {{ $rental->equipment }} - {{ $rental->contact_info }} - <img src="{{ $rental->image_url }}">
-            </a>
-        </li>   
-    @endforeach
-        </a>
-</u>
+    <div class="ml-5">
+        <h1>Current Rentals</h1>
+        <ul>
+            @foreach ($rentals as $rental)
+                <li>
+                    <a href="{{ route('rentals.show', $rental->id) }}">
+                        {{ $rental->name }} - {{ $rental->location }} - € {{ $rental->price_per_night }} per night - {{ $rental->nomber_of_rooms }} rooms - {{ $rental->description }} - {{ $rental->equipment }} - {{ $rental->contact_info }} - <img src="{{ $rental->image_url }}">
+                    </a>
+                </li>   
+            @endforeach
+        </ul>
+    </div>
 @endsection
