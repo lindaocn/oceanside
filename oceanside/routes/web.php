445edcb988fa20ecb8abc6f93d2a2112b20eb2a4
lindaocn <lilienhelpss@web.de>
2025-09-25 10:37:52 +0200

    routers update functions

--- oceanside/routes/web.php
+++ oceanside/routes/web.php
@@ -10,6 +10,18 @@ Route::get('/', function () {
 Route::prefix('rentals')->name('rentals.')->group(function () {
     // show
     Route::get('show/{id}', [RentalController::class, 'show'])->name('show');
+    // create
+    Route::get('create', [RentalController::class, 'create'])->name('create');
+    // list all
     Route::get('list', [RentalController::class, 'list_all'])->name('list');
+     // update
+    Route::get('update/{id}', [RentalController::class, 'update'])->name('update');
+    // delete
+    Route::get('delete/{id}', [RentalController::class, 'delete'])->name('delete');
+    // Store
+    Route::POST('store', [RentalController::class, 'store'])->name('store');
+    // Edit
+    Route::get('edit/{id}', [RentalController::class, 'edit'])->name('edit');
+    
 });
 
