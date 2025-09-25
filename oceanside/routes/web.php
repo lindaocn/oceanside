85903762894ffb3f2c6bb12c123c03e703228f6e
lindaocn <lilienhelpss@web.de>
2025-09-25 10:28:18 +0200

    Show router

--- oceanside/routes/web.php
+++ oceanside/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('rentals')->name('rental.')->group(function () {
+    // show
+    Route::get('show/{id}', [PokemonController::class, 'show'])->name('show');
+});
+
