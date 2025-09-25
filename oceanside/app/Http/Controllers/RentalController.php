d24f38c477359831392e2c2707385baba4e3aae9
SP-MMIBdx <stephen.pedder@mmibordeaux.com>
2025-09-25 10:24:18 +0200

    Add function show

--- oceanside/app/Http/Controllers/RentalController.php
+++ oceanside/app/Http/Controllers/RentalController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rentals;
 use Illuminate\Http\Request;
 
 class RentalController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $rentals = Rentals::findOrFail($id);
+        return view('rentals.show', compact('rentals'));
+    }
+
 }
