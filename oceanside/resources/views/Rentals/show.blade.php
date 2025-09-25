f2adf5aade18f1e04a70dd11c39a65670b4b2a9b
SP-MMIBdx <stephen.pedder@mmibordeaux.com>
2025-09-25 16:58:10 +0200

    add @mailto show.blade

--- oceanside/resources/views/Rentals/show.blade.php
+++ oceanside/resources/views/Rentals/show.blade.php
@@ -4,7 +4,7 @@
 <p>Number of Rooms:  {{ $rentals->nomber_of_rooms }}</p>
 <p>Equipment:  {{ $rentals->equipment }}</p>
 <p>Description:  {{ $rentals->description }}</p>
-<p>Contact Info:  {{ $rentals->contact_info }}</p>
+<p>Contact Info: <a href="mailto:{{ $rentals->contact_info }}"> {{ $rentals->contact_info }} </a></p>
 <img src="{{ $rentals->image_url }}" alt="{{ $rentals->name }}">
 <a href="{{ route('rentals.edit', ['id' => $rentals->id]) }}">Edit this rental</a>
 <a href="{{ route('rentals.list') }}">Back to all rentals</a>
