42a3b1fdbe80e0565e6d3857cd369be6bd601814
SP-MMIBdx <stephen.pedder@mmibordeaux.com>
2025-09-25 12:50:07 +0200

    modify edit.blade

--- oceanside/resources/views/Rentals/edit.blade.php
+++ oceanside/resources/views/Rentals/edit.blade.php
@@ -6,19 +6,19 @@
     <input type="text" id="name" name="name" value="{{ $rentals->name }}"><br><br>
 
     <label for="location">Location:</label>
-    <input type="text" id="type" name="type" value="{{ $rentals->location }}"><br><br>
+    <input type="text" id="location" name="location" value="{{ $rentals->location }}"><br><br>
 
     <label for="price_per_night">Price Per Night:</label>
-    <input type="number" id="price_per_night" name="price_per_night" value="{{ $price_per_night->price_per_night }}"><br><br>
+    <input type="number" id="price_per_night" name="price_per_night" value="{{ $rentals->price_per_night }}"><br><br>
 
     <label for="nomber_of_rooms">Number of Rooms:</label>
-    <input type="number" id="nomber_of_rooms" name="nomber_of_rooms" value="{{ $nomber_of_rooms->nomber_of_rooms }}"><br><br>
+    <input type="number" id="nomber_of_rooms" name="nomber_of_rooms" value="{{ $rentals->nomber_of_rooms }}"><br><br>
 
     <label for="equipment">Equipment:</label>
-    <input type="text" id="equipment" name="equipment" value="{{ $equipment->equipment }}"><br><br>
+    <input type="text" id="equipment" name="equipment" value="{{ $rentals->equipment }}"><br><br>
 
     <label for="description">Description:</label>
-    <input type="text" id="description" name="description" value="{{ $description->description }}"><br><br>
+    <input type="text" id="description" name="description" value="{{ $rentals->description }}"><br><br>
 
 
     <label for="image_url">Image URL:</label>
