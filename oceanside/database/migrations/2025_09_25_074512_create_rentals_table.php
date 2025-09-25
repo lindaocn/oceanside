f23803554a163379f9f312909bab27973357a2da
lindaocn <lilienhelpss@web.de>
2025-09-25 10:11:01 +0200

    Table create file rentals

--- oceanside/database/migrations/2025_09_25_074512_create_rentals_table.php
+++ oceanside/database/migrations/2025_09_25_074512_create_rentals_table.php
@@ -14,6 +14,14 @@ return new class extends Migration
         Schema::create('rentals', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('location');
+            $table->decimal('price_per_night', 8, 2);
+            $table->integer('nomber_of_rooms');
+            $table->text('description')->nullable();
+            $table->text('equipment')->nullable();
+            $table->string('image_url')->nullable();
+            // $table->softDeletes();
         });
     }
 
