0489e22f807976198801a2f38c6ca538d24fdbdc
lindaocn <lilienhelpss@web.de>
2025-09-25 12:01:14 +0200

    update all blade rentals

--- oceanside/resources/views/Rentals/all.blade.php
+++ oceanside/resources/views/Rentals/all.blade.php
@@ -1,15 +1,14 @@
 @extends('app')
-@extends('content')
+@section('content')
 
 <h1>Listing of the rentals</h1>
 <u>
-    @foreach ($rentals as $rental)
+    @foreach ($rentals as $rentals)
         <li>
-            <a href="{{ route('rentals.show', $rental->id) }}">
-                {{ $rentals->name }} - {{ $rentals->location }} - ${{ $rentals->price_per_night }} per night - {{ $rentals->number_of_rooms }} rooms - {{ $rentals->description }} - {{ $rentals->equipment }} - <img src="{{ $rentals->image_url }}">
+            <a href="{{ route('rentals.show', $rentals->id) }}">
+                {{ $rentals->name }} - {{ $rentals->location }} - ${{ $rentals->price_per_night }} per night - {{ $rentals->nomber_of_rooms }} rooms - {{ $rentals->description }} - {{ $rentals->equipment }} - <img src="{{ $rentals->image_url }}">
             </a>
         </li>   
     @endforeach
 </u>
-
 @endsection
