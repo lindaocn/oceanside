4ca74132ea603922e6a048102ec3c9fbd8145e3a
lindaocn <lilienhelpss@web.de>
2025-09-25 09:54:58 +0200

    Rentals Migration Co-authored-by: Stephen Pedder <SP-MMIBdx@users.noreply.github.com>

---
+++ oceanside/app/Models/Rentals.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rentals extends Model
+{ 
+    protected $fillable = [
+        'name',
+        'location',
+        'price_per_night',
+        'nomber_of_rooms',
+        'description',
+        'equipment',
+        'image_url',
+    ];
+}
