b1af476564b41caed40f2c0916a08c942ab33e1e
lindaocn <lilienhelpss@web.de>
2025-09-25 12:14:25 +0200

    update controller web.php

--- oceanside/routes/web.php
+++ oceanside/routes/web.php
@@ -15,13 +15,13 @@ Route::prefix('rentals')->name('rentals.')->group(function () {
     // list all
     Route::get('list', [RentalController::class, 'list_all'])->name('list');
      // update
-    Route::get('update/{id}', [RentalController::class, 'update'])->name('update');
-    // delete
     Route::get('delete/{id}', [RentalController::class, 'delete'])->name('delete');
     // Store
     Route::POST('store', [RentalController::class, 'store'])->name('store');
     // Edit
     Route::get('edit/{id}', [RentalController::class, 'edit'])->name('edit');
+    // delete
+    Route::POST('update/{id}', [RentalController::class, 'update'])->name('update');
     
 });
 
