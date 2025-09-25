1736d1a73970a2b517c6bedd58c8131ac6d9a15a
lindaocn <lilienhelpss@web.de>
2025-09-25 17:28:13 +0200

    show blade bootstrap

--- oceanside/resources/views/Rentals/all.blade.php
+++ oceanside/resources/views/Rentals/all.blade.php
@@ -1,16 +1,28 @@
-
 @extends('app')
 @section('content')
-    <div class="ml-5">
+    <div class="mt-4">
         <h1>Current Rentals</h1>
-        <ul>
+        <div class="row">
             @foreach ($rentals as $rental)
-                <li>
-                    <a href="{{ route('rentals.show', $rental->id) }}">
-                        {{ $rental->name }} - {{ $rental->location }} - € {{ $rental->price_per_night }} per night - {{ $rental->nomber_of_rooms }} rooms - {{ $rental->description }} - {{ $rental->equipment }} - {{ $rental->contact_info }} - <img src="{{ $rental->image_url }}">
-                    </a>
-                </li>   
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card h-100 border border-light shadow-sm rounded-4" style="background-color: #e3f0ff;">
+                        <img src="{{ $rental->image_url }}" class="card-img-top rounded-top-4" alt="{{ $rental->name }}">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <a href="{{ route('rentals.show', $rental->id) }}">{{ $rental->name }}</a>
+                            </h5>
+                            <p class="card-text">
+                                {{ $rental->location }}<br>
+                                € {{ $rental->price_per_night }} per night<br>
+                                {{ $rental->nomber_of_rooms }} rooms<br>
+                                {{ $rental->description }}<br>
+                                {{ $rental->equipment }}<br>
+                                {{ $rental->contact_info }}
+                            </p>
+                        </div>
+                    </div>
+                </div>
             @endforeach
-        </ul>
+        </div>
     </div>
 @endsection
