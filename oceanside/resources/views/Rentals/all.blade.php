9a42a164bdfead24f50480ae5dabe883eff1f966
lindaocn <lilienhelpss@web.de>
2025-09-25 11:56:11 +0200

    update correction all blade

--- oceanside/resources/views/Rentals/all.blade.php
+++ oceanside/resources/views/Rentals/all.blade.php
@@ -3,10 +3,10 @@
 
 <h1>Listing of the rentals</h1>
 <u>
-    @foreach ($rentals as $rentals)
+    @foreach ($rentals as $rental)
         <li>
             <a href="{{ route('rentals.show', $rental->id) }}">
-                {{ $rentals->name }} - {{ $rentals->location }} - ${{ $rentals->price_per_night }} per night - {{ $rentals->nomber_of_rooms }} rooms - {{ $rentals->description }} - {{ $rentals->equipment }} - <img src="{{ $rentals->image_url }}">
+                {{ $rentals->name }} - {{ $rentals->location }} - ${{ $rentals->price_per_night }} per night - {{ $rentals->number_of_rooms }} rooms - {{ $rentals->description }} - {{ $rentals->equipment }} - <img src="{{ $rentals->image_url }}">
             </a>
         </li>   
     @endforeach
