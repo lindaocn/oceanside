4f18889111b74cf0af77da038a8589cf50bc1280
lindaocn <lilienhelpss@web.de>
2025-09-26 12:24:18 +0200

    update dashboard page

--- oceanside/resources/views/dashboard.blade.php
+++ oceanside/resources/views/dashboard.blade.php
@@ -7,8 +7,21 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            {{-- Rentals-Teil einbinden --}}
-            @include('rentals.all', ['rentals' => $rentals])
+
+            <!-- Box für den Login-Hinweis -->
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-6">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+
+            <!-- Rentals Liste einbinden -->
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    @include('rentals.all', ['rentals' => $rentals])
+                </div>
+            </div>
+
         </div>
     </div>
 </x-app-layout>
